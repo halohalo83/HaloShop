b433e355044f1a6e12f8b14a1e05a0aa1956763c
Halo <halohalo831999@gmail.com>
2021-07-29 10:13:18 +0700

    Hoàn thiện tầng service

--- HaloShop.Data/Repositories/PostRepository.cs
+++ HaloShop.Data/Repositories/PostRepository.cs
@@ -1,16 +1,33 @@
 ﻿using HaloShop.Data.Infrastructure;
 using HaloShop.Model.Models;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace HaloShop.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
+        IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow); // dùng để hiển thị danh sách bài viết
     }
 
     public class PostRepository : RepositoryBase<Post>, IPostRepository
     {
         public PostRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+        }
+
+        public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
+        {
+            // phân trang
+            var query = from p in DbContext.Posts
+                        join pt in DbContext.PostTags
+                        on p.ID equals pt.PostID
+                        where pt.TagID == tag && p.Status 
+                        orderby p.CreatedDate descending
+                        select p;
+
+            totalRow = query.Count();
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            return query;
         }
     }
 }
